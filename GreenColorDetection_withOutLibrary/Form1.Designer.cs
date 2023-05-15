e3dc00553ef70716fa5a77582da05de510e24458
MJbidram <mjbidram1378@gmail.com>
2023-05-15 14:13:10 +0430

    firstCommit

---
+++ GreenColorDetection_withOutLibrary/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace GreenColorDetection_withOutLibrary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openImages = new System.Windows.Forms.Button();
+            this.detect = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openImages
+            // 
+            this.openImages.Location = new System.Drawing.Point(13, 13);
+            this.openImages.Name = "openImages";
+            this.openImages.Size = new System.Drawing.Size(75, 23);
+            this.openImages.TabIndex = 1;
+            this.openImages.Text = "Open";
+            this.openImages.UseVisualStyleBackColor = true;
+            this.openImages.Click += new System.EventHandler(this.openImages_Click);
+            // 
+            // detect
+            // 
+            this.detect.Location = new System.Drawing.Point(95, 13);
+            this.detect.Name = "detect";
+            this.detect.Size = new System.Drawing.Size(75, 23);
+            this.detect.TabIndex = 2;
+            this.detect.Text = "Detect";
+            this.detect.UseVisualStyleBackColor = true;
+            this.detect.Click += new System.EventHandler(this.detect_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(443, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(416, 459);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(865, 42);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(406, 459);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(416, 459);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 528);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.detect);
+            this.Controls.Add(this.openImages);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button openImages;
+        private System.Windows.Forms.Button detect;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GreenColorDetection_withOutLibrary/Form1.cs
@@ -0,0 +1,205 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+
+namespace GreenColorDetection_withOutLibrary
+{
+
+    public partial class Form1 : Form
+    {
+ 
+        Bitmap image;
+        Bitmap originalimage;
+
+
+        int grayValue;
+        int[,] matrix;
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void openImages_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(dialog.FileName);
+                originalimage = image;
+                pictureBox1.Image = image;
+            }
+            else
+            {
+                throw new Exception("No file selected.");
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GreenDetector(image);
+
+        }
+
+        public void GreenDetector(Bitmap image)
+        {
+
+            image = GreenColorThresholding(image, 100);
+
+            image = ConvertToGrayAndBlackAndWhite(image);
+
+            matrix = ConvertToMatrix(image);
+
+            DrawBoundingBox(matrix);
+
+
+        }
+
+        private void detect_Click(object sender, EventArgs e)
+        {
+            GreenDetector(image);
+
+        }
+        public static Bitmap GreenColorThresholding(Bitmap image, int threshold)
+        {
+            Bitmap result = new Bitmap(image.Width, image.Height);
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color pixelColor = image.GetPixel(i, j);
+                    if (pixelColor.G >= threshold && pixelColor.G > pixelColor.R && pixelColor.G > pixelColor.B)
+                    {
+                        result.SetPixel(i, j, Color.Green);
+                    }
+                    else
+                    {
+                        result.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+
+            return result;
+        }
+        public Bitmap ConvertToGrayAndBlackAndWhite(Bitmap image)
+        {
+            Bitmap bw = new Bitmap(image.Width, image.Height);
+
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color color = image.GetPixel(i, j);
+                    grayValue = (color.R + color.G + color.B) / 3;
+                    Color grayColor = Color.FromArgb(grayValue, grayValue, grayValue);
+                    bw.SetPixel(i, j, grayColor);
+                    Color c = bw.GetPixel(i, j);
+                    int gray = (int)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
+                    bw.SetPixel(i, j, gray < grayValue ? Color.Black : Color.White);
+
+
+                }
+            }
+            pictureBox2.Image = bw;
+            return bw;
+        }
+        public static int[,] ConvertToMatrix(Bitmap bw)
+        {
+            int width = bw.Width;
+            int height = bw.Height;
+            int[,] matrix = new int[width, height];
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color c = bw.GetPixel(i, j);
+                    matrix[i, j] = c.R == 0 ? 1 : 0;
+                }
+            }
+
+            return matrix;
+        }
+
+        public void DrawBoundingBox(int[,] matrix)
+        {
+            int width = matrix.GetLength(0);
+            int height = matrix.GetLength(1);
+            Bitmap bmp = new Bitmap(width, height);
+
+            // پیمایش تصویر با الگوریتم DFS
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    if (matrix[i, j] == 1)
+                    {
+
+                        int startX = i;
+                        int startY = j;
+
+
+                        int minX = i;
+                        int minY = j;
+                        int maxX = i;
+                        int maxY = j;
+
+
+                        Stack<(int, int)> stack = new Stack<(int, int)>();
+                        stack.Push((i, j));
+
+                        while (stack.Count > 0)
+                        {
+                            (int x, int y) = stack.Pop();
+                            if (x >= 0 && x < width && y >= 0 && y < height && matrix[x, y] == 1)
+                            {
+                                matrix[x, y] = 0;
+                                minX = Math.Min(minX, x);
+                                minY = Math.Min(minY, y);
+                                maxX = Math.Max(maxX, x);
+                                maxY = Math.Max(maxY, y);
+                                stack.Push((x + 1, y));
+                                stack.Push((x - 1, y));
+                                stack.Push((x, y + 1));
+                                stack.Push((x, y - 1));
+                            }
+                        }
+
+                        // رسم خط قرمز دور شکل های سیاه
+                        for (int k = minX; k <= maxX; k++)
+                        {
+                            originalimage.SetPixel(k, minY, Color.Red);
+                            originalimage.SetPixel(k, maxY, Color.Red);
+                        }
+                        for (int k = minY; k <= maxY; k++)
+                        {
+                            originalimage.SetPixel(minX, k, Color.Red);
+                            originalimage.SetPixel(maxX, k, Color.Red);
+                        }
+                    }
+                }
+            }
+            pictureBox3.Image = originalimage;
+
+
+        }
+
+
+    }
+
+}
